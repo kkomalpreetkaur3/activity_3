0a60b82ccde75ca2781a7931c449242bde0e366d
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2025-10-16 18:13:42 -0500

    Added PenaltyStrategy for handling incomplete payments with penalty

--- patterns/strategy/penalty_strategy.py
+++ patterns/strategy/penalty_strategy.py
@@ -1,4 +1,21 @@
 """This module defines the PenaltyStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "komalpreet Kaur"
+__version__ = "1.0.0"
+
+from .payment_strategy import PaymentStrategy
+
+class PenaltyStrategy(PaymentStrategy):
+    """Applies a $10 penalty for incomplete payments."""
+
+    def process_payment(self, billing_account, payee, amount):
+
+        billing_account.deduct_balance(payee, amount)
+        balance = billing_account.get_balance(payee)
+
+        if balance <= 0:
+            return f"Processed payment of ${amount:.2f}. New balance: $0.00."
+        else:
+            billing_account.add_balance(payee, 10.00)
+            new_balance = billing_account.get_balance(payee)
+            return f"Insufficient payment. Added penalty fee of $10.00. New balance: ${new_balance:.2f}."
