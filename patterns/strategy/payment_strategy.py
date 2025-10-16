5d6afeb04e02c8ff43de85930e630a0f330f5946
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2025-10-16 17:56:33 -0500

    Implement PaymentStrategy abstract base class

--- patterns/strategy/payment_strategy.py
+++ patterns/strategy/payment_strategy.py
@@ -1,4 +1,14 @@
 """This module defines the PaymentStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Komalpreet Kaur"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+
+class PaymentStrategy(ABC):
+    """Abstract superclass for all payment strategies."""
+
+    @abstractmethod
+    def process_payment(self, billing_account, payee, amount):
+        """All concrete strategies must implement this method."""
+        pass 
